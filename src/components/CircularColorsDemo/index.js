c0f5c7b694dbdfabc47eafb519b4df6eb4c37070
Bonnie Schulkin <biz@bonnieschulkin.com>
2023-08-11 11:51:54 -0700

    Exercise 6 and 7

--- src/components/CircularColorsDemo/index.js
+++ src/components/CircularColorsDemo/index.js
@@ -1,2 +1,4 @@
-export * from './CircularColorsDemo';
-export { default } from './CircularColorsDemo';
+import dynamic from "next/dynamic";
+
+// load dynamically every time
+export default dynamic(() => import("./CircularColorsDemo"));
--- src/helpers/mdx-components.js
+++ src/helpers/mdx-components.js
@@ -1,9 +1,11 @@
+import CircularColorsDemo from "@/components/CircularColorsDemo";
 import DivisionGroupsDemo from "@/components/DivisionGroupsDemo";
 import CodeSnippet from "@/components/CodeSnippet";
 
 const COMPONENT_MAP = {
   pre: CodeSnippet,
   DivisionGroupsDemo,
+  CircularColorsDemo,
 };
 
 export default COMPONENT_MAP;
